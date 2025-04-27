fa5113305462c8729b553063a7fad26488e50dce
Daniil <s33932@pjwstk.edu.pl>
2025-04-27 15:39:37 +0200

    Final commit

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    public void sleep (){
+        System.out.println("Animal is sleeping");
+    }
+    public void makeNoise(){
+        System.out.println("Animal is making noise");
+    }
+    public void roam() {
+        System.out.println("Animal is roaming");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,24 @@
 public class Main {
     public static void main(String[] args) {
 
+      Car car = new Car(6);
+      car.start();
+      car.stop();
+      System.out.println("Number of seats in this car: " + car.getNumberOfSeats());
+
+      System.out.println();
+      System.out.println("Sound of animals :");
+
+      Lion lion = new Lion();
+      Cat cat = new Cat();
+      Wolf wolf = new Wolf();
+      Dog dog = new Dog();
+      lion.makeNoise();
+      cat.makeNoise();
+      wolf.makeNoise();
+      dog.makeNoise();
+
     }
+
+
 }
